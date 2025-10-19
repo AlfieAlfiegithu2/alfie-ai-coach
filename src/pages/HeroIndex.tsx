8c9ae00b46e37979cb007b3fd0df00ee7eff165e
Alfie <alfie@Alfies-MacBook-Air.local>
2025-10-20 01:15:10 +0800

    fix: Improve image container positioning to eliminate background bleed-through
    - Add relative positioning to image containers
    - Position images absolutely with inset-0 to fill entire container
    - Ensure white background covers all areas completely
    - Eliminate any potential transparency or background visibility issues

--- src/pages/HeroIndex.tsx
+++ src/pages/HeroIndex.tsx
@@ -534,40 +534,40 @@ const HeroIndex = () => {
           <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 mt-8">
             {/* IELTS Card */}
             <div className="text-center">
-              <div className="w-32 h-32 mx-auto mb-4 overflow-hidden rounded-lg bg-white">
-                <img src="/IELTS.png" alt="IELTS" className="w-full h-full object-contain" />
+              <div className="w-32 h-32 mx-auto mb-4 overflow-hidden rounded-lg bg-white relative">
+                <img src="/IELTS.png" alt="IELTS" className="w-full h-full object-contain absolute inset-0" />
               </div>
               <h3 className="text-xl font-semibold text-gray-800 font-nunito">{getText(['programs', 'cards', 'ielts', 'title'])}</h3>
             </div>
 
             {/* TOEFL Card */}
             <div className="text-center">
-              <div className="w-32 h-32 mx-auto mb-4 overflow-hidden rounded-lg bg-white">
-                <img src="/Generated Image October 19, 2025 - 11_14PM.png" alt="TOEFL" className="w-full h-full object-contain" />
+              <div className="w-32 h-32 mx-auto mb-4 overflow-hidden rounded-lg bg-white relative">
+                <img src="/Generated Image October 19, 2025 - 11_14PM.png" alt="TOEFL" className="w-full h-full object-contain absolute inset-0" />
               </div>
               <h3 className="text-xl font-semibold text-gray-800 font-nunito">{getText(['programs', 'cards', 'toefl', 'title'])}</h3>
             </div>
 
             {/* TOEIC Card */}
             <div className="text-center">
-              <div className="w-32 h-32 mx-auto mb-4 overflow-hidden rounded-lg bg-white">
-                <img src="/REAL TOEIC.png" alt="TOEIC" className="w-full h-full object-contain" />
+              <div className="w-32 h-32 mx-auto mb-4 overflow-hidden rounded-lg bg-white relative">
+                <img src="/REAL TOEIC.png" alt="TOEIC" className="w-full h-full object-contain absolute inset-0" />
               </div>
               <h3 className="text-xl font-semibold text-gray-800 font-nunito">{getText(['programs', 'cards', 'toeic', 'title'])}</h3>
             </div>
 
             {/* PTE Card */}
             <div className="text-center">
-              <div className="w-32 h-32 mx-auto mb-4 overflow-hidden rounded-lg bg-white">
-                <img src="/PTE.png" alt="PTE" className="w-full h-full object-contain" />
+              <div className="w-32 h-32 mx-auto mb-4 overflow-hidden rounded-lg bg-white relative">
+                <img src="/PTE.png" alt="PTE" className="w-full h-full object-contain absolute inset-0" />
               </div>
               <h3 className="text-xl font-semibold text-gray-800 font-nunito">{getText(['programs', 'cards', 'pte', 'title'])}</h3>
             </div>
 
             {/* General English Card */}
             <div className="text-center">
-              <div className="w-32 h-32 mx-auto mb-4 overflow-hidden rounded-lg bg-white">
-                <img src="/REEAL GENERAL ENGLISH.png" alt="General English" className="w-full h-full object-contain" />
+              <div className="w-32 h-32 mx-auto mb-4 overflow-hidden rounded-lg bg-white relative">
+                <img src="/REEAL GENERAL ENGLISH.png" alt="General English" className="w-full h-full object-contain absolute inset-0" />
               </div>
               <h3 className="text-xl font-semibold text-gray-800 font-nunito">{getText(['programs', 'cards', 'general', 'title'])}</h3>
             </div>
